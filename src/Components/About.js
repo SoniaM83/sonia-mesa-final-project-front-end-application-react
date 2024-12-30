86f4cd6a6e44247df170c30bc06532ab3d0dd585
SoniaM83 <Sonia Mesa>
2024-12-30 12:59:34 -0800

    added highlight

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
         <div id="about">
             <h2>About</h2>
-            <p>Use this app to create and review flashcards. Navigate through cards using the buttons, and add your own using the form below.</p>
+            <p>Use this app to create and review flashcards. Navigate through cards using the buttons, 
+                and add your own using the form in the <span className="highlight">Flashcards</span> section.</p>
         </div>
     );
 };
