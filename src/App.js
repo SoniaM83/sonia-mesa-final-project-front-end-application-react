663daef1e66a2e99fddc38ac37c853f9efa66d7c
SoniaM83 <Sonia Mesa>
2025-01-16 20:19:50 -0800

    updated localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; //REACT AND HOOKS
+import React, { useState, useEffect, useRef } from 'react'; //REACT AND HOOKS
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import FlashcardInput from './Components/FlashcardInput';
@@ -9,9 +9,22 @@ import './App.css';
 function App() {
   const [currentView, setCurrentView] = useState('home');
   const [flashcards, setFlashcards] = useState([]); //STORES FLASHCARDS
+  const isInitialMount = useRef(true); //TRACKS INITIAL MOUNT
 
   useEffect(() => {
+    const savedFlashcards = localStorage.getItem('flashcards'); //GET FLASHCARDS FROM LOCAL STORAGE
+    if (savedFlashcards) {
+      console.log('Loaded from localStorage', JSON.parse(savedFlashcards));
+      setFlashcards(JSON.parse(savedFlashcards));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false; //SKIP SAVING ON FIRST MOUNT
+    } else {
     localStorage.setItem('flashcards', JSON.stringify(flashcards)); //SAVE FLASHCARDS TO LOCAL STORAGE
+    }
   }, [flashcards]);
 
   const addFlashcard = (question, answer) => { //ADD NEW FLASHCARD
